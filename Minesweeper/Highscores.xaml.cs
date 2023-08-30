4fe34c48477f79d53d137378fe8b33c79539585a
Diogo <dacunha.138120@e.unavarra.es>
2023-08-30 17:49:16 +0200

    [#2] Changed the saving system from file to SQLite

--- Minesweeper/Highscores.xaml.cs
+++ Minesweeper/Highscores.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.IO;
+using System.Data.SQLite;
+using System.Data;
 
 namespace Minesweeper
 {
@@ -21,48 +23,75 @@ namespace Minesweeper
     /// </summary>
     public partial class Highscores : Window
     {
-        private const string HighscoresFilePath = "highscores.json";
+        private readonly SQLiteConnection conn;
 
         public Highscores(int score, Difficulty difficulty)
         {
             InitializeComponent();
 
-            if (score < GetHighScore(difficulty) || GetHighScore(difficulty) == 0)
+            conn = new SQLiteConnection("Data Source=highscores.db;New=True;");
+            conn.Open();
+
+            CreateTable();
+
+            int bestScore = GetHighScore(difficulty);
+            if (score < bestScore || bestScore == 0)
             {
                 UpdateHighScore(score, difficulty);
+                bestScore = score;
             }
 
-            labelBestScore.Content = "Your best time was: " + Timer.ConvertMilisecondsToTime(GetHighScore(difficulty));
+            labelBestScore.Content = "Your best time was: " + Timer.ConvertMilisecondsToTime(bestScore);
             labelActualScore.Content = "Your time was: " + Timer.ConvertMilisecondsToTime(score);
+            conn.Close();
         }
 
-        public static void UpdateHighScore(int score, Difficulty difficulty)
+        private void CreateTable()
         {
-            var highscores = LoadHighscores();
-            highscores[difficulty.ToString()] = score;
-            SaveHighscores(highscores);
+            var command = conn.CreateCommand();
+            command.CommandText =
+                @"
+                CREATE TABLE IF NOT EXISTS highscores (
+                    time int,
+                    difficulty int
+                );
+                INSERT OR IGNORE INTO highscores (time, difficulty) VALUES (0, 1), (0, 2), (0, 3);
+                ";
+            command.ExecuteNonQuery();
         }
 
-        public static int GetHighScore(Difficulty difficulty)
+        private void UpdateHighScore(int score, Difficulty difficulty)
         {
-            var highscores = LoadHighscores();
-            return highscores.TryGetValue(difficulty.ToString(), out int score) ? score : 0;
+            var command = conn.CreateCommand();
+            command.CommandText =
+                @"
+                UPDATE highscores
+                SET time = $score
+                WHERE difficulty = $difficulty
+                ";
+            command.Parameters.AddWithValue("$difficulty", difficulty);
+            command.Parameters.AddWithValue("$score", score);
+
+            command.ExecuteNonQuery();
         }
 
-        private static void SaveHighscores(Dictionary<string, int> highscores)
+        private int GetHighScore(Difficulty difficulty)
         {
-            string json = JsonConvert.SerializeObject(highscores, Formatting.Indented);
-            File.WriteAllText(HighscoresFilePath, json);
-        }
+            var command = conn.CreateCommand();
+            command.CommandText =
+                @"
+                SELECT time
+                FROM highscores
+                WHERE difficulty = $difficulty
+                ";
+            command.Parameters.AddWithValue("$difficulty", difficulty);
 
-        private static Dictionary<string, int> LoadHighscores()
-        {
-            if (File.Exists(HighscoresFilePath))
-            {
-                string json = File.ReadAllText(HighscoresFilePath);
-                return JsonConvert.DeserializeObject<Dictionary<string, int>>(json);
-            }
-            return new Dictionary<string, int>();
+            var reader = command.ExecuteReader();
+
+            reader.Read();
+            int score = reader.GetInt32(0);
+
+            return score;
         }
     }
 }
